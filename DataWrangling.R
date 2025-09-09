22a4754f98476521f7d3b7d1230feb5ba19e30a1
Brian K <brk828@gmail.com>
2025-09-09 16:40:00 -0700

    First update with data wrangling and post-stocking survival scripts.

---
+++ DataWrangling.R
@@ -0,0 +1,226 @@
+# Data Wrangling for genetic backwater analysis IP 1-6 and Yuma Cove BW
+# B. Kesner September 2025
+
+# Set parameters
+SurvivalDAL <- 90 # minimum days a fish is at large after tagging to be considered "survived"
+SizeClass2 <- 350 # TL (mm) cutoff for Size Class 2
+SizeClass3 <- 500 # TL (mm) cutoff for Size Class 3
+
+# Load useful lab functions
+source("c:/GIT/RDependencies/LabFunctions.R")
+
+packages(dplyr)     # data manipulation
+packages(lubridate) # date and time manipulation
+packages(readxl) # import Excel spreadsheets
+packages(data.table) # faster at indexing than grouping in dplyr
+packages(openxlsx) # package openxlsx is required to create to Excel files
+
+# Load BWScanning data workspace or downlod and load if more than 7 days old
+if(file.exists("data/BWScanningIndex.RData")){
+  data_info <- file.info("data/BWScanningIndex.RData")
+  data_date <- as.Date(data_info$mtime)
+  if(data_date>Sys.Date() - 7){
+    load("data/BWScanningIndex.RData")
+  } else {
+    download_backwater("data")
+    load("data/BWScanningIndex.RData")
+  }
+} else {
+  download_backwater("data")
+  load("data/BWScanningIndex.RData")
+}
+
+# Load NFWG data workspace or downlod and load if more than 7 days old
+if(file.exists("data/NFWGAnalysis.RData")){
+  data_info <- file.info("data/NFWGAnalysis.RData")
+  data_date <- as.Date(data_info$mtime)
+  if(data_date>Sys.Date() - 7){
+    load("data/NFWGAnalysis.RData")
+  } else {
+    download_nfwg("data")
+    load("data/NFWGAnalysis.RData")
+  }
+} else {
+  download_nfwg("data")
+  load("data/NFWGAnalysis.RData")
+}
+
+rm(split_hourly, download_nfwg, download_basin, download_backwater)
+  
+# Restrict PITindex dataframe to study backwaters
+StudyBWContacts <- BWContacts %>%
+  filter(LID > 1042 & LID < 1049| LID == 592) %>%
+  mutate(ScanHr = hour(DateTime),
+         ScanMonth = month(DateTime)) %>% 
+  select(EID, PIT, PITIndex, Date, ScanHr, ScanMonth, Location, Species = species, tagging_date) 
+
+# Use data.tables to reduce contacts to summary per PIT-Date-Hour
+StudyBWContactsdt <- as.data.table(StudyBWContacts)
+
+StudyBWContacts <- StudyBWContactsdt[
+  , .(count = .N), 
+  by = .(EID, Location, Species, PIT, PITIndex, Date, ScanHr, ScanMonth, tagging_date)] %>%
+  as.data.frame() %>%
+  mutate(ScanFY = as.integer(ifelse(ScanMonth > 9, year(Date)+1, year(Date))),
+         ScanMonthName = format(Date, "%b"))
+
+rm(StudyBWContactsdt)
+
+StudyBWNFWG <- NFWGAnalysis %>%
+  filter(location_id > 1042 & location_id < 1049| location_id == 592) 
+
+StudyBWEffort <- BWEffort %>%
+  filter(LID > 1042 & LID < 1049| LID == 592) %>%
+  mutate(ScanMonth = month(MidDate),
+         ScanMonthName = format(MidDate, "%b"),
+         DeployedHrs = as.numeric(difftime(Retrieve, Deploy, units = "hours"))) %>%
+  select(EID, Location, Deploy, Retrieve, Issue, UnitType, MidDate, 
+         Comments, ScanTimeHrs, DeployedHrs, ScanMonth, ScanMonthName) %>%
+  left_join(BWContacts %>% 
+              select(EID, PIT, Date) %>% 
+              group_by(EID) %>%
+              summarise(Contacts = n(), MinScan = min(Date), MaxScan = max(Date)) %>%
+              ungroup(), by = "EID") %>%
+  mutate(Contacts = no_na_df(Contacts)) %>%
+  filter(Contacts > 0, !is.na(MinScan)) %>%
+  mutate(EffectiveScanHrs = as.numeric(difftime(MaxScan, MinScan, units = "hours")),
+         ScanFY = as.integer(ifelse(ScanMonth > 9, year(MidDate)+1, year(MidDate))))
+
+EffectiveTimeLong <- StudyBWEffort %>%
+  filter(EffectiveScanHrs - DeployedHrs > 24)
+
+# Identify contact PIT records without a NFWG entry (no PITIndex)
+# Ignore short-term contacts and recent releases that have not been added to database
+ContactsNoNFWG <- StudyBWContacts %>%
+  filter(is.na(PITIndex), Date < as.Date("2024-10-01")) %>%
+  group_by(PIT) %>%
+  summarise(Backwater = min(Location), MinScanDate = min(Date), MaxScanDate = max(Date), contacts = sum(count)) %>%
+  ungroup() %>%
+  mutate(DAL = as.numeric(difftime(MaxScanDate, MinScanDate, units = "days"))) %>%
+  filter(DAL > SurvivalDAL)
+
+
+rm(BWContacts, NFWGAnalysis) # cleanup
+
+# Summary of contact information for tags with a NFWG record
+ContactsSummary <- StudyBWContacts %>%
+  filter(!is.na(PITIndex)) %>%
+  group_by(PITIndex, Location) %>%
+  summarise(MinScanDate = min(Date), MaxScanDate = max(Date), contacts = sum(count)) %>%
+  ungroup() %>%
+  mutate(ScanDAL = as.numeric(difftime(MaxScanDate, MinScanDate, units = "days"))) 
+
+# Fish hopping ponds or improperly allocated scan data records
+ContactSummaryDuplicateLocations <- ContactsSummary %>%
+  group_by(PITIndex) %>%
+  summarise(count = n(), FirstLocation = min(Location), SecondLocation = max(Location)) %>%
+  ungroup() %>%
+  filter(count > 1) %>%
+  left_join(StudyBWContacts %>%
+              select(PITIndex, FirstEID = EID, Location, FirstDate = Date), 
+            by = c("PITIndex", "FirstLocation" = "Location")) %>%
+  group_by(PITIndex, FirstLocation, SecondLocation) %>%
+  summarise(FirstContacts = n(), FirstDate = min(FirstDate), FirstEID = min(FirstEID)) %>%
+  ungroup() %>%
+  left_join(StudyBWContacts %>% select(PITIndex, SecondEID = EID, Location, SecondDate = Date), 
+            by = c("PITIndex", "SecondLocation" = "Location")) %>%
+  group_by(PITIndex, FirstLocation, SecondLocation, FirstEID, FirstContacts, FirstDate) %>%
+  summarise(SecondContacts = n(), SecondDate = min(SecondDate), SecondEID = min(SecondEID)) %>%
+  ungroup()
+
+# Dataframe of first tagging (capture tagged or stocked)
+# Cannot rely on pit1_new alone as some fish were tagged in hatchery
+# Add summary contact data and calculate tagging DAL
+StudyBWNFWGTagging <- StudyBWNFWG %>% 
+  filter(pit1_new == "yes"| event == "stocking") %>%
+  select(collection_date, location, disposition, event, fin_clip, primary_method,
+         species, PITIndex, sex, total_length, tagging_date, location_id) %>%
+  left_join(ContactsSummary, by = c("PITIndex", "location" = "Location")) %>%
+  mutate(MaxDAL = ifelse(!is.na(MaxScanDate), 
+                as.numeric(difftime(MaxScanDate, collection_date, units = "days")),
+                0), 
+         contacts = no_na_df(contacts)) %>%
+  select(-ScanDAL) %>%
+  mutate(Survived = ifelse(MaxDAL > SurvivalDAL, 1, 0),
+         SurvivedFY24 = ifelse(!is.na(MaxScanDate) & 
+                                 MaxScanDate > as.Date("2024-09-30"), 1, 0))
+
+# Check for duplicates, there should be none
+TaggingDuplicates <- StudyBWNFWGTagging %>%
+  group_by(PITIndex) %>%
+  summarise(count = n()) %>%
+  ungroup() %>%
+  filter(count >1)
+
+# Look for Tags with an NFWG record but no tagging record
+ContactNoTagging <- ContactsSummary %>%
+  anti_join(StudyBWNFWGTagging, by = "PITIndex") %>%
+  filter(ScanDAL > SurvivalDAL)
+
+# Fish holdovers scanned during study period but released prior
+StudyBWNFWGTaggingHoldover <- StudyBWNFWGTagging %>%
+  filter(collection_date < as.Date("2013-01-01"),
+         MaxScanDate > as.Date("2013-01-01"))
+
+# Create workbook for contacts with NO PITIndex
+wb <- createWorkbook() # creates object to hold workbook sheets
+addWorksheet(wb, "ContactsNoNFWG") # add worksheet
+writeData(wb, "ContactsNoNFWG", ContactsNoNFWG) # write dataframe
+
+addWorksheet(wb, "BadScanUploads") # add worksheet
+writeData(wb, "BadScanUploads", EffectiveTimeLong) # write dataframe
+
+addWorksheet(wb, "PondHoppers") # add worksheet
+writeData(wb, "PondHoppers", ContactSummaryDuplicateLocations) # write dataframe
+
+addWorksheet(wb, "ScannedWithoutTagging") # add worksheet
+writeData(wb, "ScannedWithoutTagging", ContactNoTagging) # write dataframe
+
+addWorksheet(wb, "Holdovers") # add worksheet
+writeData(wb, "Holdovers", StudyBWNFWGTaggingHoldover) # write dataframe
+
+saveWorkbook(wb, paste0("output/BWIssues",
+                        format(Sys.time(), "%Y%m%d"), ".xlsx"), overwrite = TRUE)
+
+
+# filter out older tagging records
+StudyBWNFWGTagging <- StudyBWNFWGTagging %>%
+  filter(collection_date > as.Date("2013-01-01") & 
+           location_id == 592|
+           collection_date > as.Date("2016-01-01") & 
+           location_id > 1042 & 
+           location_id < 1049)
+
+# Summarize all tagged fish
+BackwaterSummary <- StudyBWNFWGTagging %>%
+  group_by(species, location, collection_date, event, disposition, sex) %>%
+  summarise(count = n(), meanTL = as.integer(mean(total_length)), survivedDAL = sum(Survived),
+            survivedFY24 = sum(SurvivedFY24)) %>%
+  ungroup()
+
+# Summary for stockings only
+StockingBackwaterSummary <- BackwaterSummary %>%
+  filter(event == "stocking", collection_date < as.Date("2024-10-01")) %>%
+  rename(release_date = collection_date)
+
+ScanningBackwaterSummary <- StudyBWContacts %>%
+  arrange(Location, ScanFY) %>%
+  group_by(Location, ScanFY) %>%
+  summarise(Contacts = n(), Uniques = n_distinct(PITIndex)) %>%
+  ungroup()
+
+# Create workbook for contacts with NO PITIndex
+wb <- createWorkbook() # creates object to hold workbook sheets
+addWorksheet(wb, "StockingBackwaterSummary") # add worksheet
+writeData(wb, "StockingBackwaterSummary", StockingBackwaterSummary) # write dataframe
+
+addWorksheet(wb, "ScanningBackwaterSummary") # add worksheet
+writeData(wb, "ScanningBackwaterSummary", ScanningBackwaterSummary) # write dataframe
+
+saveWorkbook(wb, paste0("output/StockingBackwaterSummary",
+                        format(Sys.time(), "%Y%m%d"), ".xlsx"), overwrite = TRUE)
+
+save(StudyBWNFWG, StudyBWNFWGTagging,StudyBWEffort, StudyBWContacts, SurvivalDAL, 
+     SizeClass2, SizeClass3, no_na, no_na_df, packages,
+     file = "data/ReportingData.RData")
+
---
+++ PostStockingSurvival.R
@@ -0,0 +1,156 @@
+# Post-stocking survival analysis for IP 1-6 and Yuma Cove Backwater
+# B. Kesner September 2025
+
+# Mark-recapture based on Months since release with FY of release, Sex, Size, and Backwater as factors
+# Logistic regression based on SurvivalDAL as well
+
+load("data/ReportingData.RData")
+packages(dplyr)     # data manipulation
+packages(lubridate) # date and time manipulation
+packages(glmmTMB) # General linear mixed model analysis built on TMB automatic differentiation engine
+packages(ggplot2) # Plotting
+
+StockingBW <- StudyBWNFWGTagging %>%
+  filter(event == "stocking", collection_date < as.Date("2024-10-01"), sex != "U") %>%
+  rename(ReleaseDate = collection_date) %>%
+  mutate(ReleaseFY = as.factor(ifelse(month(ReleaseDate) > 9, year(ReleaseDate)+1, year(ReleaseDate))),
+         ReleaseMonth = as.integer(month(ReleaseDate)),
+         ReleaseMonthName = format(ReleaseDate, "%b"),
+         ReleaseMonthFY = ifelse(ReleaseMonth>9, ReleaseMonth-9, ReleaseMonth+3),
+         sex = as.factor(trimws(sex)),
+         location = as.factor(location),
+         TLClass = as.integer(total_length*.1)*10) 
+
+StockingBWSurvivalSummary <- StockingBW %>%
+  group_by(location, location_id, ReleaseFY, species, sex, TLClass) %>%
+  summarise(Released = n(), Survivors = sum(Survived), ContactedProp = Survivors/Released) %>%
+  ungroup() %>%
+  filter(Released >= 5)
+
+# Yuma stocking in 2020 did not have proper sexing done
+YumaStocking <- StockingBW %>%
+  filter(location_id == 592, ReleaseFY != 2020) %>%
+  select(PITIndex, ReleaseFY, sex, total_length, Survived, SurvivedFY24)
+
+YumaReleaseFY <- unique(YumaStocking$ReleaseFY)
+#zero inflated model for Yuma Cove backwater.
+DALModelYuma <- glmmTMB(Survived ~ sex + total_length * ReleaseFY,
+                    family = binomial(link = 'logit'), 
+                    data = YumaStocking)
+
+#creating a predictions data.frame from model for graphing
+PredictorsYuma <- expand.grid(total_length = as.integer(seq(350, 500, by = 10)), 
+                          ReleaseFY =YumaReleaseFY,
+                          sex = as.factor(c("M", "F")))
+
+PredictionsYuma <- predict(DALModelYuma, newdata = PredictorsYuma, type = "response", se.fit = TRUE)
+lowerCI <- PredictionsYuma$fit - (1.96 * PredictionsYuma$se.fit)
+upperCI <- PredictionsYuma$fit + (1.96 * PredictionsYuma$se.fit)
+fit <- PredictionsYuma$fit
+PredictedYuma <- cbind(PredictorsYuma, fit, lowerCI, upperCI)
+
+# cleanup
+rm(PredictorsYuma, PredictionsYuma, lowerCI, upperCI, fit)
+
+# Yuma Figure
+YumaDALSurvival <- ggplot(PredictedYuma, 
+                     aes(x = total_length, y = fit, color = sex)) + 
+  geom_line(linewidth = 1) + 
+  labs(x = 'Total Length (mm)', y = 'Detection Probability', color = 'Sex') + 
+  theme(plot.margin = margin(.75,.75,.75,.75, unit = 'cm'), 
+        axis.title.x = element_text(vjust = -2), axis.title.y = element_text(vjust = 5)) +
+  geom_ribbon(aes(x = total_length, ymin = lowerCI, ymax = upperCI, fill = sex), alpha = 0.3) + 
+  facet_wrap(~ReleaseFY, nrow=3) +
+  geom_point(data = StockingBWSurvivalSummary %>%
+               filter(location_id == 592, ReleaseFY != 2020, TLClass >=350), 
+             aes(x = TLClass, y = ContactedProp)) 
+
+
+YumaDALSurvival
+
+# IP XYTE stockings 
+IPXYTEStocking <- StockingBW %>%
+  filter(location_id != 592, species == "XYTE") %>%
+  select(location, PITIndex, ReleaseFY, sex, total_length, Survived, SurvivedFY24)
+
+IPXYTELocations <- unique(IPXYTEStocking$location)
+
+#zero inflated model for IP XYTE stockings
+DALModelIPXYTE <- glmmTMB(Survived ~ sex + total_length * location,
+                        family = binomial(link = 'logit'), 
+                        data = IPXYTEStocking)
+
+PredictorsIPXYTE <- expand.grid(total_length = as.integer(seq(350, 500, by = 10)), 
+                              location = IPXYTELocations,
+                              sex = as.factor(c("M", "F")))
+
+PredictionsIPXYTE <- predict(DALModelIPXYTE, newdata = PredictorsIPXYTE, type = "response", se.fit = TRUE)
+lowerCI <- PredictionsIPXYTE$fit - (1.96 * PredictionsIPXYTE$se.fit)
+upperCI <- PredictionsIPXYTE$fit + (1.96 * PredictionsIPXYTE$se.fit)
+fit <- PredictionsIPXYTE$fit
+PredictedIPXYTE <- cbind(PredictorsIPXYTE, fit, lowerCI, upperCI)
+
+# cleanup
+rm(PredictionsIPXYTE, PredictorsIPXYTE, lowerCI, upperCI, fit)
+
+# IPXTYE figure
+IPXYTEDALSurvival <- ggplot(PredictedIPXYTE, 
+                          aes(x = total_length, y = fit, color = sex)) + 
+  geom_line(linewidth = 1) + 
+  labs(x = 'Total Length (mm)', y = 'Detection Probability', color = 'Sex') + 
+  theme(plot.margin = margin(.75,.75,.75,.75, unit = 'cm'), 
+        axis.title.x = element_text(vjust = -2), axis.title.y = element_text(vjust = 5)) +
+  geom_ribbon(aes(x = total_length, ymin = lowerCI, ymax = upperCI, fill = sex), alpha = 0.3) + 
+  facet_wrap(~location, nrow=3) +
+  geom_point(data = StockingBWSurvivalSummary %>%
+               filter(location_id != 592, species == "XYTE", TLClass >=350), 
+             aes(x = TLClass, y = ContactedProp))
+
+IPXYTEDALSurvival
+
+# IP GIEL 2017 stockings 
+IPGIEL2017Stocking <- StockingBW %>%
+  filter(location_id != 592, species == "GIEL", ReleaseFY == 2017) %>%
+  select(location, PITIndex, ReleaseFY, sex, total_length, Survived, SurvivedFY24)
+
+IPGIEL2017Locations <- unique(IPGIEL2017Stocking$location)
+
+#zero inflated model for IP GIEL 2017
+DALModelIPGIEL2017 <- glmmTMB(Survived ~ sex + total_length * location,
+                          family = binomial(link = 'logit'), 
+                          data = IPGIEL2017Stocking)
+
+PredictorsIPGIEL2017 <- expand.grid(total_length = as.integer(seq(230, 290, by = 5)), 
+                                location = IPGIEL2017Locations,
+                                sex = as.factor(c("M", "F")))
+
+PredictionsIPGIEL2017 <- predict(DALModelIPGIEL2017, newdata = PredictorsIPGIEL2017, type = "response", se.fit = TRUE)
+lowerCI <- PredictionsIPGIEL2017$fit - (1.96 * PredictionsIPGIEL2017$se.fit)
+upperCI <- PredictionsIPGIEL2017$fit + (1.96 * PredictionsIPGIEL2017$se.fit)
+fit <- PredictionsIPGIEL2017$fit
+PredictedIPGIEL2017 <- cbind(PredictorsIPGIEL2017, fit, lowerCI, upperCI)
+
+# cleanup
+rm(PredictionsIPGIEL2017, PredictorsIPGIEL2017, lowerCI, upperCI, fit)
+
+# IPGIEL 2017 figure
+IPGIEL2017DALSurvival <- ggplot(PredictedIPGIEL2017, 
+                            aes(x = total_length, y = fit, color = sex)) + 
+  geom_line(linewidth = 1) + 
+  labs(x = 'Total Length (mm)', y = 'Detection Probability', color = 'Sex') + 
+  theme(plot.margin = margin(.75,.75,.75,.75, unit = 'cm'), 
+        axis.title.x = element_text(vjust = -2), axis.title.y = element_text(vjust = 5)) +
+  geom_ribbon(aes(x = total_length, ymin = lowerCI, ymax = upperCI, fill = sex), alpha = 0.3) + 
+  facet_wrap(~location, nrow=3) +
+  geom_point(data = StockingBWSurvivalSummary %>%
+               filter(location_id != 592, species == "GIEL", ReleaseFY == 2017), 
+             aes(x = TLClass, y = ContactedProp))
+
+IPGIEL2017DALSurvival
+
+ContactsFYMonthly <- StudyBWContacts %>%
+  mutate(ScanMonthFY = ifelse(ScanMonth>9, ScanMonth-9, ScanMonth+3)) %>%
+  arrange(Location, ScanFY, ScanMonthFY) %>%
+  group_by(Location, ScanFY, ScanMonthFY, ScanMonthName) %>%
+  summarise(Contacts = n(), Uniques = n_distinct(PITIndex)) %>%
+  ungroup()
